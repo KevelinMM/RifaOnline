2c82758c61f20bfcfda48b70575030c6b4e8ba71
Uniplac <github@uniplaclages.edu.br>
2023-12-21 10:34:31 -0300

    home

---
+++ src/pages/components/BuyNumbers.js
@@ -0,0 +1,16 @@
+export default function Numbers() {
+  return (
+    <div className="buy">
+      <h2>
+        Compre seu número por apenas <span className="price">R$5,00</span>
+      </h2>
+
+      <div className="numbers">
+        <div className="description">
+          <p>Selecione os números a serem comprados*</p>
+          <h4>R$5,00/número</h4>
+        </div>
+      </div>
+    </div>
+  );
+}

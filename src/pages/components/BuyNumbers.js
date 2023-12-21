43a0f61f52c8dcdd07f056890a1e44aae6159f89
Uniplac <github@uniplaclages.edu.br>
2023-12-21 16:52:25 -0300

    inicio pargina BuyNumbers

--- src/pages/components/BuyNumbers.js
+++ src/pages/components/BuyNumbers.js
@@ -10,21 +10,30 @@ export default function Numbers() {
   };
 
   return (
-    <div className="buy">
-      <h2>
-        Compre seu número por apenas <span className="price">R$5,00</span>
-      </h2>
+    <div className="content">
+      <div className="buy">
+        <h2>
+          Compre seu número por apenas <span className="price">R$5,00</span>
+        </h2>
 
-      <div className="numbers">
-        <div className="description">
-          <div>
+        <div className="numbers_content">
+          <div className="description">
             <p>Selecione os números a serem comprados*</p>
             <h4>R$5,00/número</h4>
           </div>
 
-          <ul className="number-list">{renderNumberList()}</ul>
+          <ul className="numbers">{renderNumberList()}</ul>
+          <div className="numbers_footer">
+            <button className="button_number">
+              Nenhum número(s) selecionado(s)
+            </button>
+            <p className="numbers_description">
+              *Selecione quantos números desejar
+            </p>
+          </div>
         </div>
-      </div>
+      </div>{" "}
+      <img className="studens_img" src="students.png"></img>
     </div>
   );
 }
